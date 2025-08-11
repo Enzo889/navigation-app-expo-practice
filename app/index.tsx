571891c928fc6060dcc9dbd8b11ded991df33e82
Enzo Bustamante <enzobustamante895@gmail.com>
2025-08-11 17:48:27 -0300

    Eliminar archivos y componentes no utilizados, y agregar soporte para NativeWind y Tailwind CSS.

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center  bg-rose-300">
+      <Text className="text-2xl text-rose-700">
+        App with some love for sure ❤️
+      </Text>
+    </View>
+  );
+};
+export default App;
